93904bd1299d7b4f578abc5e212a0916f9d15959
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2022-09-28 23:01:09 -0300

    Acrescentando funcionalidades à calculadora de estoque de livros

--- src/CalculadoraDeEstoque.java
+++ src/CalculadoraDeEstoque.java
@@ -1,2 +1,15 @@
 public class CalculadoraDeEstoque {
+
+    public static void main(String [] args){
+        double livroJava8;
+        double livroTDD;
+
+        livroJava8 = 58.50;
+        livroTDD = 60.10;
+
+        double soma;
+        soma = livroJava8 + livroTDD;
+
+        System.out.println("O estoque é de R$"+ soma+" em livros");
+    }
 }
